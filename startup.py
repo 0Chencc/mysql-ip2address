875ba597841f3999b8c8282b73a619e6abedc255
0chen <admin@hi-ourlife.com>
2021-08-15 15:25:04 +0800

    Update startup.py

--- startup.py
+++ startup.py
@@ -14,7 +14,8 @@ def usage():
         '-t --table chose table表名\n' \
         '-i --ipfiled the filed in selected database ip在该表中的字段名\n' \
         '-u --userid the useridfiled in selected database 用户id在该表中的字段名\n' \
-        '-m --mode'
+        '-m --mode 1.normal 2.ntoa The inet_ntoa function converts an (Ipv4) Internet network address into an ASCII string in Internet standard dotted-decimal format.\n'
+        '人话：模式1 正常的ipv4地址查归属地，模式2 将ip戳（就是那一串数字）转化为常规的ip地址'
         )
 def connectDB():
     conn = pymysql.connect(host=host,port=port,user=username,password=password)
@@ -52,7 +53,7 @@ if __name__ == '__main__':
             useridn = value
         elif opt in ('-i','--ip'):
             ip = value
-
-    if mode == '1':
-        print(f'db is {db},table is {table},mode is {mode},useridn is {useridn},ip is {ip}')
-    getipaddress(useridn=useridn,ip=ip,table=table,conn=conn)
+    # 
+    # if mode == '1':
+    #     print(f'db is {db},table is {table},mode is {mode},useridn is {useridn},ip is {ip}')
+    getipaddress(useridn=useridn,ip=ip,table=table,conn=conn,mode=mode)
