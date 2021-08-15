1193ac6fa7d1ca70953ba83778a70f7d4f35ff6e
0chen <admin@hi-ourlife.com>
2021-08-15 21:36:19 +0800

    Update ip.py

--- ip.py
+++ ip.py
@@ -5,21 +5,25 @@ cz = QQwry()
 cz.load_file('qqwry.dat', loadindex=True)
 
 
-def getipaddress(useridn, ip, table, conn,mode):
+def getipaddress(useridn, ip, table, conn,mode,db):
     cur = conn.cursor(pymysql.cursors.SSDictCursor)
-    cur.execute(f'select count(*) as iscreate from information_schema.columns where table_name = \'{table}\' and column_name = \'{ip}_address\'')
+
+    print(f'select count(*) as iscreate from information_schema.columns where table_name = \'{table}\' and column_name = \'{ip}_address\' and TABLE_SCHEMA={db}')
+    cur.execute(
+        f'select count(*) as iscreate from information_schema.columns where table_name = \'{table}\' and column_name = \'{ip}_address\' and TABLE_SCHEMA={db}')
     iscreate = cur.fetchall()
     print(iscreate[0])
     if iscreate[0]['iscreate'] == 0:
         cur.execute(f'alter table {table} add {ip}_address varchar(50) default \'\' null after {ip};')
         conn.commit()
-    if mode == 1:
+    if mode == '1':
         cur.execute(f'select {useridn},{ip} from {table}')
-    elif mode ==2:
+    elif mode =='2':
         cur.execute(f'select {useridn},inet_ntoa({ip}) as {ip} from {table}')
     data = cur.fetchall()
     for i in data:
         if i[f'{ip}'] != '':
+            address = ''
             try:
                 address = "".join(cz.lookup(i[f'{ip}']))
             except:
