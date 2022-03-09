ccba9826a4d0015b51523e3e11d3b967da9afcd7
0chen <admin@hi-ourlife.com>
2022-03-09 14:10:56 +0800

    新增了ip库启动更新
    新增了ip库启动更新

--- ip.py
+++ ip.py
@@ -1,35 +1,31 @@
 from qqwry import QQwry
+from qqwry import updateQQwry
 import pymysql
-from phone import Phone
-cz = QQwry()
-cz.load_file('qqwry.dat', loadindex=True)
+q = QQwry()
+ret = updateQQwry('qqwry.dat')
+q.load_file('qqwry.dat', loadindex=True)
 
 
-def getipaddress(useridn, ip, table, conn,mode,db):
+def ip2address(useridn, ip, table, conn, mode, db):
     cur = conn.cursor(pymysql.cursors.SSDictCursor)
     print(f'select count(*) as iscreate from information_schema.columns where table_name = \'{table}\' and column_name = \'{ip}_address\' and TABLE_SCHEMA=\'{db}\'')
     cur.execute(
         f'select count(*) as iscreate from information_schema.columns where table_name = \'{table}\' and column_name = \'{ip}_address\' and TABLE_SCHEMA=\'{db}\'')
-    iscreate = cur.fetchall()
-    print(iscreate[0])
-    if iscreate[0]['iscreate'] == 0:
+    create = cur.fetchall()
+    print(create[0])
+    if create[0]['iscreate'] == 0:
         cur.execute(f'alter table {table} add {ip}_address varchar(50) default \'\' null after {ip};')
         conn.commit()
     if mode == '1':
         cur.execute(f'select {useridn},{ip} from {table}')
-    elif mode =='2':
+    elif mode == '2':
         cur.execute(f'select {useridn},inet_ntoa({ip}) as {ip} from {table}')
     data = cur.fetchall()
     for i in data:
         if i[f'{ip}'] != '':
-            address = ''
             try:
-                address = "".join(cz.lookup(i[f'{ip}']))
-            except:
-                print(i[f'{ip}'])
-                pass
-            userid = i[f'{useridn}']
-            try:
+                address = "".join(q.lookup(i[f'{ip}']))
+                userid = i[f'{useridn}']
                 cur.execute(f'update {table} set {ip}_address = \'{address}\' where {useridn} = {userid}')
             except:
                 pass
@@ -37,4 +33,3 @@ def getipaddress(useridn, ip, table, conn,mode,db):
     cur.close()
     conn.close()
     print('finsh!')
-
--- startup.py
+++ startup.py
@@ -1,11 +1,13 @@
 import getopt
 import sys
 import pymysql
-from ip import getipaddress
+from ip import ip2address
 host = 'localhost'
 port = 3306
 username = 'root'
 password = '12345678'
+
+
 def usage():
     print(
         '-h --help help\n' \
@@ -17,45 +19,44 @@ def usage():
         '-m --mode 1.normal 2.ntoa The inet_ntoa function converts an (Ipv4) Internet network address into an ASCII string in Internet standard dotted-decimal format.\n'
         '人话：模式1 正常的ipv4地址查归属地，模式2 将ip戳（就是那一串数字）转化为常规的ip地址'
         )
+
+
 def connectDB():
-    conn = pymysql.connect(host=host,port=port,user=username,password=password)
+    conn = pymysql.connect(host=host, port=port, user=username, password=password)
     return conn
+
 
 if __name__ == '__main__':
     conn = connectDB()
-    db =''
+    db = ''
     table = ''
     ip = ''
     useridn = ''
     mode = 0 #0.select sql 1.getipaddress
     try:
-        opts,args = getopt.getopt(sys.argv[1:],
-                                  '-h-d:-a:-t:-i:-u:-m:',
-                                  ['help','database=','area=','table=','ipfiled=','userid=','mode='])
+        opts, args = getopt.getopt(sys.argv[1:],
+                                   '-h-d:-a:-t:-i:-u:-m:',
+                                   ['help', 'database=', 'area=', 'table=', 'ipfiled=', 'userid=', 'mode='])
     except getopt.GetoptError as err:
         usage()
         sys.exit()
-    for opt,value in opts:
-        if opt in ('-h','--help'):
+    for opt, value in opts:
+        if opt in ('-h', '--help'):
             usage()
             sys.exit()
-        elif opt in ('-d','--database'):
+        elif opt in ('-d', '--database'):
             db = value
             conn.select_db(db)
-        elif opt in ('-a','--area'):
+        elif opt in ('-a', '--area'):
             area = value
             print(value)
-        elif opt in ('-m','--mode'):
-            mode=value
-        elif opt in ('-t','--table'):
+        elif opt in ('-m', '--mode'):
+            mode = value
+        elif opt in ('-t', '--table'):
             table = value
-        elif opt in ('-u','--userid'):
+        elif opt in ('-u', '--userid'):
             useridn = value
-        elif opt in ('-i','--ip'):
+        elif opt in ('-i', '--ip'):
             ip = value
-    if mode in ('1','2'):
-        getipaddress(useridn=useridn, ip=ip, table=table, conn=conn, mode=mode,db=db)
-
-    #
-    # if mode == '1':
-    #     print(f'db is {db},table is {table},mode is {mode},useridn is {useridn},ip is {ip}')
+    if mode in ('1', '2'):
+        ip2address(useridn=useridn, ip=ip, table=table, conn=conn, mode=mode, db=db)
